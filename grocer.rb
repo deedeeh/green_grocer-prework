8de6cb9a83cedbf696aa50b8fc104f6f841365c7
Dina Hafez <dinahafez.8@gmail.com>
2018-08-05 16:59:31 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -57,4 +57,5 @@ def checkout(cart, coupons)
   consolidated_cart = consolidate_cart(cart)
   applied_coupons = apply_coupons(consolidated_cart, coupons)
   applied_clearance = applied_clearance(applied_coupons)
+  binding.pry
 end
