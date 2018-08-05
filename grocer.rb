478684cd816741355a854a2554a127f9d283fc83
Dina Hafez <dinahafez.8@gmail.com>
2018-08-05 17:16:48 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -57,7 +57,6 @@ def checkout(cart, coupons)
   consolidated_cart = consolidate_cart(cart)
   applied_coupons = apply_coupons(consolidated_cart, coupons)
   applied_clearance = apply_clearance(applied_coupons)
-  binding.pry
   applied_clearance.each do |k, v|
     total += v[:price] * v[:count]
   end
