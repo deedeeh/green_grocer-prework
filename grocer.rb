06f6082463bb9725f34fd29e6ab9d9f3d956019f
Dina Hafez <dinahafez.8@gmail.com>
2018-08-05 16:36:41 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -1,13 +1,54 @@
+require 'pry'
+
 def consolidate_cart(cart)
   # code here
+  new_hash = {}
+  cart.each do |groceries|
+    groceries.each do |item, item_data|
+      if new_hash[item] == nil
+        new_hash[item] = {}
+        new_hash[item] = item_data
+        new_hash[item][:count] = 1
+      else
+        new_hash[item][:count] += 1
+      end
+    end
+  end
+  new_hash
 end
 
 def apply_coupons(cart, coupons)
   # code here
+  result = {}
+  cart.each do |item, item_data|
+    result[item] = item_data
+    coupons.each do |coupon|
+      if item == coupon[:item] && item_data[:count] >= coupon[:num]
+        item_data[:count] -= coupon[:num]
+        if result["#{item} W/COUPON"] != nil 
+          result["#{item} W/COUPON"][:count] += 1 
+        else 
+          result["#{item} W/COUPON"] = {
+            :price => coupon[:cost],
+            :clearance => item_data[:clearance],
+            :count => 1
+          }
+        end
+      end
+    end
+  end
+  result
 end
 
 def apply_clearance(cart)
   # code here
+  result = {}
+  cart.each do |item, item_data|
+    if item[:clearance] == true
+      item[:price] = item[:price] - (item[:price] - 0.2) 
+    end
+  end
+  result
 end
 
 def checkout(cart, coupons)
